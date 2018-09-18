f242e622c595a25acbf531fc3422f8b803d022f2
Halldór Rúnar <halldorv18@ru.is>
2018-09-18 09:21:35 +0000

    A new repo is born

---
+++ tile_traveller.py
@@ -0,0 +1,6 @@
+#Player starts in tile 1,1. 
+#Travel directions pop up whenever player enters a new square (the ways he can go.)
+#Input should be (N)orth (up), (E)ast (right), (S)outh (up), (W)est (left).
+#"Invalid directions" is printed when user inputs an invalid directions.
+#Instructions to winning the maze: (3x3 tiles) 1,1 -- > 1,2 ; 1,2 --> 1,3 or 2,2 or 1,1 ; 1,3 --> 2,3 or 2,2; 2,3 --> 3,3; 3,3 --> 3,2; 3,2 --> 3,1 (Victory tile).
+#User can always backtrack, unless he has gotten to the victory tile. 
