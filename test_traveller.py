903feb5a38ae074e6b1ee68d84127480bf29784a
Halldór Rúnar <halldorv18@ru.is>
2018-09-21 15:39:15 +0000

    Looks ready

--- test_traveller.py
+++ test_traveller.py
@@ -7,3 +7,56 @@
 #Github-id: https://github.com/Halldorv18/tile_traveller
 
 x, y = 1, 1
+victory = False
+options = "ESWN"
+
+print("You can travel: (N)orth.")
+while victory == False:
+    chose = input("Direction: ")
+    chose = chose.upper()
+
+    if chose in options:
+    
+        if chose == "E":
+            x -=1
+        elif chose == "S":
+            y -=1
+        elif chose == "W":
+            x +=1
+        elif chose == "N":
+            y +=1
+    else:
+        print("Not a valid direction!")
+        continue
+
+    if x == 3 and y == 1:
+            victory = True
+            print("Victory!")
+            break
+    else:
+           
+        if x == 1 and y == 1:
+            options = "N"
+            string = "(N)orth."
+        elif x == 1 and y == 2:
+            options = "NES"
+            string= "(N)orth or (E)ast or (S)outh."
+        elif x == 1 and y == 3:
+            options = "ES"
+            string = "(E)ast or (S)outh."
+        elif x == 2 and y == 1:
+            options = "N"
+            string = "(N)orth."
+        elif x == 2 and y == 2:
+            options = "SW"
+            string = "(S)outh or (W)est."
+        elif x == 2 and y == 3:
+            options = "EW"
+            string = "(E)ast or (W)est."
+        elif x == 3 and y == 2:
+            options = "NS"
+            string = "(N)orth or (S)outh."
+        elif x == 3 and y == 3:
+            options = "SW"
+            string = "(S)outh or (W)est."
+        print("You can travel:", string)
